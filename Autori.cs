e2e77af9b9c12ed4ea05da388b8e2c014c4f6cd5
Adam <45467414+adambillekvist@users.noreply.github.com>
2022-02-28 19:48:37 +0100

    Add files via upload
    Autori solved

---
+++ Autori.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Autori
+{
+    class Autori
+    {
+        static void Main(string[] args)
+        {
+            var names = EnterNames();
+
+            string resultat = "";
+            for (int i = 0; i < names.Length; i++)
+            {
+                resultat = resultat + FirstChar(names[i]);
+            }
+
+            Console.WriteLine(resultat);
+
+        }
+        private static string FirstChar(string str)
+        {
+            return str[0].ToString();
+
+
+        }
+        private static string[] EnterNames()
+        {
+            return Console.ReadLine().Split('-');
+        }
+        
+    }           
+}
+   
